dc0876671567f574c38f6441fbaa105c66d9e5a0
jackchealdang <jackchealdang@gmail.com>
2024-01-29 23:45:35 -0600

    added proper JS validation!

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-function validateEmail() {
-    const fname = document.forms.signup.fname.value;
-    const lname = document.forms.signup.lname.value;
-    const email = document.forms.signup.email;
-    const password = document.forms.signup.password.value;
+const form = document.getElementById('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-    console.log(fname, lname, email, password);
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-    const constraint = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
-    console.log(constraint);
+    validateInputs();
+});
 
-    if (constraint.test(email.value)) {
-        email.setCustomValidity("");
-    } else {
-        email.setCustomValidity("Enter a valid email!");
-    }
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-    return false;
-
-    return true;
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
 }
 
-window.onload = () => {
-    document.getElementById("email").oninput = validateEmail;
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (fnameValue === '') {
+        setError(fname, 'First name is required');
+    } else {
+        setSuccess(fname);
+    }
+
+    if (lnameValue === '') {
+        setError(lname, 'Last name is required');
+    } else {
+        setSuccess(lname);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 characters');
+    } else {
+        setSuccess(password);
+    }
 }
